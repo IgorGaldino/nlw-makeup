4a30ea54516021fc940bea057d46dba1e260014f
Igor Martins Galdino <igormgaldino@gmail.com>
2021-03-01 16:13:52 -0300

    Fixed ExperienceBar

--- src/contexts/CallengesContext.tsx
+++ src/contexts/CallengesContext.tsx
@@ -31,9 +31,9 @@ interface ChallengesProviderProps {
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
 export function ChallengesProvider({ children, ...rest }: ChallengesProviderProps) {
-  const [level, setLevel] = useState(rest.level ?? 1);
-  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
-  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
+  const [level, setLevel] = useState(!isNaN(rest.level) ? rest.level : 1);
+  const [currentExperience, setCurrentExperience] = useState(!isNaN(rest.currentExperience) ? rest.currentExperience : 0);
+  const [challengesCompleted, setChallengesCompleted] = useState(!isNaN(rest.challengesCompleted) ? rest.challengesCompleted : 0);
 
   const [activeChallenge, setActiveChallenge] = useState(null);
   const [isLevelUpModalOpen, setIsLevelModalOpen] = useState(false);
